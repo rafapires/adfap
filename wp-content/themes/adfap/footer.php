8696163bdd6e44155cd6af25c90cbd75cd8b388f
Rafael Pires <rafael.pires.sp@gmail.com>
2013-08-12 15:11:08 -0300

    configuração inicial do novo tema

---
+++ wp-content/themes/adfap/footer.php
@@ -0,0 +1,17 @@
+    <hr>
+
+      <footer>
+        <p>© Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+    <!-- Le javascript
+    ================================================== -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+
+  </body>
+</html>
