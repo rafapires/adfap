cf7702a02ceea36d22e9e4651fd0993821d616d3
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-03 17:13:46 -0300

    ajusta layout da pagina administração de condomínios

--- wp-content/themes/adfap/condominio.php
+++ wp-content/themes/adfap/condominio.php
@@ -1,41 +1,126 @@
 <div class="row">
-	<div class="span8">
+	<div class="span6">
 		<img class="pull-right" src="http://localhost/adfap/wp-content/uploads/2013/09/parceria-predios.jpg" />
-		Ser um real parceiro do síndico é nosso objetivo, muito mais do que administrar a contabilidade, pessoal, pagamentos e recebimentos, criamos uma relação pro-ativa junto ao síndico, conselheiros e moradores, levando soluções e oportunidades que fazem sentido para as características únicas de seu condomínio.
+		<p>Ser um real parceiro do síndico é nosso objetivo, muito mais do que administrar a contabilidade, pessoal, pagamentos e recebimentos, criamos uma relação pro-ativa junto ao síndico, conselheiros e moradores, levando soluções e oportunidades que fazem sentido para as características únicas de seu condomínio.</p>
+		<p>Através da internet, o síndico tem total controle sobre seu condomínio, acompanhando em tempo real cada ação de nossa dedicada equipe.</p>
+		<p>Através do site exclusivo do condomínio cada morador dispõe de uma série de recursos que facilitam a interação com seus vizinhos, contato com o síndico e conselheiros e também acessam todas as informações contábeis, documentos e atas com total transparência.</p>
+
+
 	</div>
-	<div class="span4">
+	<div class="span6">
 		<div class="well pull-right">
 			<blockquote>Entender as caracterísitcas únicas do seu condomínio é o que nos permite oferecer uma administração eficiente e ágil.</blockquote>
 		</div>
 		<h2>Serviços especializados</h2>
-		<ul>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Contabilidade</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Departamento Pessoal</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Assessoria Jurídica</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Apoio total ao síndico</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Cobrança</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Contas a pagar</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Recebimento</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Cadastro de moradores</a>
-			</li>
-			<li>
-				<a class="btn btn-block btn-warning" href="#">Cadastro de fornecedores</a>
-			</li>
-		</ul>
+		<div class="accordion" id="accordion1">
+	        <div class="accordion-group">
+				<div class="accordion-heading">
+                	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv1">
+						Contabilidade
+                    </a>
+				</div>
+				<div id="serv1" class="accordion-body collapse in">
+					<div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</div>
+				</div>
+            </div>
+            <div class="accordion-group">
+				<div class="accordion-heading">
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv2">
+						Departamento Pessoal
+                    </a>
+				</div>
+				<div id="serv2" class="accordion-body collapse">
+					<div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+			</div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv3">
+						Assessoria Judídica
+                    </a>
+				</div>
+				<div id="serv3" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv4">
+						Apoio total ao Síndico
+                    </a>
+				</div>
+				<div id="serv4" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv5">
+						Cobrança
+                    </a>
+				</div>
+				<div id="serv5" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv6">
+						Contas a Pagar
+                    </a>
+				</div>
+				<div id="serv6" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv7">
+						Recebimentos
+                    </a>
+				</div>
+				<div id="serv7" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv8">
+						Controle de moradores
+                    </a>
+				</div>
+				<div id="serv8" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#serv9">
+						Controle de fornecedores
+                    </a>
+				</div>
+				<div id="serv9" class="accordion-body collapse">
+                    <div class="accordion-inner">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+				</div>
+            </div>
+		</div>
 	</div>
 </div>
