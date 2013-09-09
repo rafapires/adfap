492b469dc1c6820cd9da75624a368c451b052218
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-09 14:41:09 -0300

    insere código da athos no form do header

--- wp-content/themes/adfap/header.php
+++ wp-content/themes/adfap/header.php
@@ -29,10 +29,12 @@
               <h1 class="text-center">Cuidando do seu bem como se fosse nosso</h1>
             </div>
             <div class="span5">
-              <form class="form-inline pull-right">
-                <input type="text" class="input-small" placeholder="Email">
-                <input type="password" class="input-small" placeholder="Senha">
-                <button type="submit" class="btn btn-small">Ok</button>
+              <form class="form-inline pull-right" action="http://webware.com.br/bin/login.asp" method="post">
+                <input type="hidden" name="urlErro" value="http://www.webware.com.br/bin/administradora/default.asp?adm=18545180&amp;msg=ERRO">
+                <input type="hidden" name="a" value="18545180">
+                <input type="text" name="mem" class="input-small" placeholder="Email">
+                <input type="password" name="pass" class="input-small" placeholder="Senha">
+                <button type="submit" name="submit" class="btn btn-small">Entrar</button>
               </form>
             </div>
           </div>
