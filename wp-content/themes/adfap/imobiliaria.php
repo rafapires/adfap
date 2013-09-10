ba9df9c757a7b0900080c3664fe273adce8148da
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-10 00:06:47 -0300

    edita texto de imobiliaria.php

--- wp-content/themes/adfap/imobiliaria.php
+++ wp-content/themes/adfap/imobiliaria.php
@@ -1,13 +1,12 @@
 	<div class="hero-unit">
 		<h1>Sua solução definitiva em imóveis</h1>
 		Se você está querendo <strong>vender</strong> ou está querendo <strong>alugar</strong> seu imóvel, a ADFAP conta com corretores altamente capacitados e treinados no uso das redes sociais para achar um cliente o mais rápido possível. Atuamos em toda São Paulo.
-<!--		<h3 class="label label-success pull-right">Escolha abaixo</h3>-->
 	</div>
 <div class="row-fluid">
-	<div class="well span6">
-		Nossos corretores utilizam nossas ferramentas de divulgação social para divulgar seu imóvel para o público certo e nosso departamento jurídico faz uma criteriosa avaliação do comprador ou locador para que você não tenha nenhuma dor de cabeça.
+	<div class="well span3">
+		Nossos corretores utilizam nossas ferramentas de redes sociais para divulgar seu imóvel para o público certo e nosso departamento jurídico faz uma criteriosa avaliação do comprador ou locador para que você não tenha nenhuma dor de cabeça.
 	</div>
-	<div class="well span6">
-		formulário
+	<div class="well span9">
+		[formidable id=7]
 	</div>
 </div>
