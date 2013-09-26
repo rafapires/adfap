f26610e8361907fa5436663709555da93ef8fdaf
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-26 11:26:11 -0300

    correções texto feitos pela Paula

--- wp-content/themes/adfap/home.php
+++ wp-content/themes/adfap/home.php
@@ -1,25 +1,24 @@
 <!-- Main hero unit for a primary marketing message or call to action -->
 <div class="hero-unit">
-	<h1>Adiministrando seu condomínio de um jeito diferente</h1>
-	Vivemos em um mundo novo, uma época de conectividade, velocidade e colaboração. A ADFAP tem profissionais capacitados, tecnologia e uma administração moderna e voltada ao cliente para cumprir com as atuais necessidades de nossos clientes. 
+	<h1>Administrando seu Condomínio de um jeito diferente</h1>
+	Vivemos em um mundo novo, uma época de conectividade, velocidade e colaboração. A ADFAP possui profissionais capacitados, tecnologia e uma administração moderna para cumprir com as atuais necessidades de nossos clientes.
 	<a class="btn btn-primary btn-large pull-right">Saiba mais »</a>
-
 </div>
 <!-- Example row of columns -->
 <div class="row">
 	<div class="span4">
-		<h2>Condomínios</h2>
-		Administrar seu condomínio é muito mais do que enviar os boletos, pagar as contas, elaborar relatórios e participar de reuniões. Para a ADFAP, administra seu condomínio é uma oportunidade de colaborar para as melhores soluções, indicar oportunidades e ajudar o síndico a fortalecer o patrimônio e criar um ambiente harmonioso para seus moradores.
+		<h2>Administração de Condomínios</h2>
+		Administrar seu condomínio é muito mais do que enviar os boletos, pagar as contas, elaborar relatórios e participar de reuniões. Para a ADFAP, administrar seu condomínio é uma oportunidade de encontrar as melhores soluções e ajudar o síndico a fortalecer o patrimônio e criar um ambiente harmonioso para seus moradores.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
 	<div class="span4">
-		<h2>Venda e Locação</h2>
-		Ajudamos você encontrar aquele imóvel perfeito para seu momento de vida. Se você for proprietário, administramos a locação do seu imóvel com apurado critério de avaliação do locador e processos específicos para proteger seu patrimônio.
+		<h2>Compra,Venda e Locação</h2>
+		Ajudamos você a comprar,vender ou alugar com segurança e rapidez. Administramos a locação do seu imóvel com apurado critério de avaliação do locador ou comprador, com processos específicos para proteger seu patrimônio.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
 	<div class="span4">
 		<h2>Assessoria Jurídica</h2>
-		Nosso departamento jurídico próprio é altamente qualificado em todas as áreas que envolvem o mercado imobiliário e administração de condomínios, com especialização em direito trabalhista, imobiliário, familiar e etc...
+		Nosso departamento jurídico próprio é altamente qualificado em todas as áreas que envolvem o mercado imobiliário e administração de condomínios, com especialização em direito imobiliário e trabalhista.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
 </div>
--- wp-content/themes/adfap/imobiliaria.php
+++ wp-content/themes/adfap/imobiliaria.php
@@ -1,13 +1,10 @@
-	<div class="hero-unit">
-		<h1>Sua solução definitiva em imóveis</h1>
-		Se você está querendo <strong>vender</strong> ou está querendo <strong>alugar</strong> seu imóvel, a ADFAP conta com corretores altamente capacitados e treinados no uso das redes sociais para achar um cliente o mais rápido possível. Atuamos em toda São Paulo.
-	</div>
+<div class="hero-unit">
+	<h1>Sua solução definitiva em imóveis</h1>
+	Se você está querendo <strong>vender</strong> ou está querendo <strong>alugar</strong> seu imóvel, a ADFAP conta com corretores altamente capacitados e treinados no uso das redes sociais para achar um cliente o mais rápido possível. Atuamos em toda São Paulo.
+</div>
 <div class="row-fluid">
-	<div class="texto well span3">
-		Nossos corretores utilizam nossas ferramentas de<br>redes sociais para divulgar seu imóvel para o público certo e nosso departamento jurídico faz uma criteriosa avaliação do comprador ou locador para que você não tenha nenhuma<br>dor de cabeça.
+	<div class="texto well span3">Nossos corretores utilizam nossas ferramentas de redes sociais para divulgar seu imóvel para o público certo e nosso departamento jurídico faz uma criteriosa avaliação do comprador ou locador para que você não tenha nenhuma dor de cabeça.
 		<div class="well well-warning">Sem compromisso, preencha o formulário com apenas 3 passos.</div>
 	</div>
-	<div class="form well span9">
-		[formidable id=7]
-	</div>
+	<div class="form well span9">[formidable id=8]</div>
 </div>
