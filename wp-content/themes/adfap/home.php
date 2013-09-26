582af095973a0685c74110b6e862239f0016c754
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-26 12:51:14 -0300

    faz pequenos ajustes de layout no call-to-action da home

--- wp-content/themes/adfap/home.php
+++ wp-content/themes/adfap/home.php
@@ -5,19 +5,19 @@
 	<a class="btn btn-primary btn-large pull-right">Saiba mais »</a>
 </div>
 <!-- Example row of columns -->
-<div class="row">
+<div class="row call-to-action">
 	<div class="span4">
-		<h2>Administração de Condomínios</h2>
+		<h2 class="cor1">Administração de Condomínios</h2>
 		Administrar seu condomínio é muito mais do que enviar os boletos, pagar as contas, elaborar relatórios e participar de reuniões. Para a ADFAP, administrar seu condomínio é uma oportunidade de encontrar as melhores soluções e ajudar o síndico a fortalecer o patrimônio e criar um ambiente harmonioso para seus moradores.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
 	<div class="span4">
-		<h2>Compra,Venda e Locação</h2>
+		<h2 class="cor2">Compra,Venda e Locação</h2>
 		Ajudamos você a comprar,vender ou alugar com segurança e rapidez. Administramos a locação do seu imóvel com apurado critério de avaliação do locador ou comprador, com processos específicos para proteger seu patrimônio.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
 	<div class="span4">
-		<h2>Assessoria Jurídica</h2>
+		<h2 class="cor3">Assessoria Jurídica</h2>
 		Nosso departamento jurídico próprio é altamente qualificado em todas as áreas que envolvem o mercado imobiliário e administração de condomínios, com especialização em direito imobiliário e trabalhista.
 		<a class="btn pull-right" href="#">Saiba mais »</a>
 	</div>
