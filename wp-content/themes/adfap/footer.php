f9a739adbec4550a0739d610420ac63089dd682e
Rafael Pires <rafael.pires.sp@gmail.com>
2013-09-27 11:22:58 -0300

    cria um footer simples com endereço e telefone

--- wp-content/themes/adfap/footer.php
+++ wp-content/themes/adfap/footer.php
@@ -1,11 +1,15 @@
     <hr>
 
-      <footer>
-        <p>© Company 2012</p>
-      </footer>
+
 
     </div> <!-- /container -->
-
+    <footer class="dados">
+      <div class="row">
+        <span class="pull-left">ADFAP © Company 2013</span>
+        <span>Praça João Mendes, 42 Cj.79 - Tel: (11)3106-4817</span>
+        <span class="pull-right">desenvolvido por <a href="http://br.linkedin.com/in/rafaelpiressp/" target="_blank">Rafael Pires</a></span>
+      </div>
+    </footer>
     <?php wp_footer(); ?>
 
     <!-- Le javascript
