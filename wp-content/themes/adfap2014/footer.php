3ec5af36ecfc7a8fd10e7107c2d394a9012e30dd
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-06-29 02:56:17 -0300

    cria a home em HTML

---
+++ wp-content/themes/adfap2014/footer.php
@@ -0,0 +1,38 @@
+	<section id="footer">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>Coluna 1</h3>
+			</div>
+			<div class="col-sm-4">
+				<h3>Coluna 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis eleifend quam sit amet tincidunt. Donec quis venenatis volutpat.</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>Clouna 3</h3>
+			</div>
+		</div>
+		<hr />
+		<div class="row">
+	        <div class="span3">
+    	        <span class="pull-left">ADFAP © Company 2013</span>
+        	</div>
+			<div class="span6 text-center">
+	            <span>Praça João Mendes, 42 Cj.79 - CEP:01500-001 - CENTRO - São Paulo - SP<br>Tel: (11)3106-4817</span>
+    	    </div>
+        	<div class="span3">
+            	<span class="pull-right">desenvolvido por <a href="http://br.linkedin.com/in/rafaelpiressp/" target="_blank">Rafael Pires</a></span>
+    	    </div>
+		</div>	
+    </section>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  
+
+</body></html>
