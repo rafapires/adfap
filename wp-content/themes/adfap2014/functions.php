93acebea7d99edd6f1d64a053438ffbf9a99edc2
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-07-01 15:29:31 -0300

    resolve bug thumbnails #5

--- wp-content/themes/adfap2014/functions.php
+++ wp-content/themes/adfap2014/functions.php
@@ -1,5 +1,5 @@
 <?php 
-//remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_content', 'wpautop' );
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 150, 150, true ); 
 
