26c3a0d0f567efc4d67a5823b1e0a8827b70b6b6
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-07-06 00:48:47 -0300

    acerta meio

--- wp-content/themes/adfap2014/page-adm.php
+++ wp-content/themes/adfap2014/page-adm.php
@@ -36,8 +36,47 @@ get_header();
 
 
 </section>
-<section id="meio" class="col-sm-7">
-meio
+<section id="meio" class="col-sm-7 col-sm-offset-2">
+	<h1>Administradora de Condomínio</h1>
+	<!-- conteúdo -->
+
+	<strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.
+	<h2>Header Level 2</h2>
+	<ol>
+		<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+		<li>Aliquam tincidunt mauris eu risus.</li>
+	</ol>
+	<pre><code>
+	#header h1 a { 
+		display: block; 
+		width: 300px; 
+		height: 80px; 
+	}
+	</code></pre>
+	Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+
+	<img src="http://lorempixum.com/300/200" alt="Random image courtesy of LoremPixum.com" />
+	<h2>Header Level 2</h2>
+	<ol>
+		<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+		<li>Aliquam tincidunt mauris eu risus.</li>
+	</ol>
+	<pre><code>
+	#header h1 a { 
+		display: block; 
+		width: 300px; 
+		height: 80px; 
+	}
+	</code></pre>
+	<pre><code>
+	#header h1 a { 
+		display: block; 
+		width: 300px; 
+		height: 80px; 
+	}
+	</code></pre>
+
+
 </section>
 <section id='direita' class='col-sm-3'>
 direita
