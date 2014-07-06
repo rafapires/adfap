9a5f0cc7a63e03ae73c14bd1aa2cf54e32c5f08f
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-07-06 00:34:08 -0300

    acerta coluna da esquerda e detalhes de menu como cor seta mouse hover e etc...

--- wp-content/themes/adfap2014/page-adm.php
+++ wp-content/themes/adfap2014/page-adm.php
@@ -8,11 +8,11 @@ get_header();
 <section id="esquerda" class="col-xp-12 col-sm-2">
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
+	<a class="logo center-block" href="#">
+  		<img src="<?php bloginfo('template_url'); ?>/img/logo-web-hi.png" class="img-responsive center-block">
+	</a>
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-		<a class="navbar-brand" href="#">
-      		<img src="<?php bloginfo('template_url'); ?>/img/logo-web-hi.png" class="img-responsive center-block">
-		</a>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -24,10 +24,10 @@ get_header();
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		<ul class="nav nav-pills nav-stacked">
-			<li class="active">home</li>
-			<li>item 1</li>
-			<li>item 2</li>
-			<li>item 3</li>
+			<li class="active"><a hreh='#'>home</a></li>
+			<li><a href="#">item 1</a></li>
+			<li><a href="#">item 2</a></li>
+			<li><a href="#">item 3</a></li>
 		</ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
