c5e75eec8db8e60cc288a531a10156eaef12849f
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-07-14 11:51:47 -0300

    cria pagina da proposta em HTML close #11

---
+++ wp-content/themes/adfap2014/page-proposta.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: proposta
+*/
+get_header();
+?>
+
+<section id="esquerda" class="col-xp-12 col-sm-2">
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+	<a class="logo center-block" href="#">
+  		<img src="<?php bloginfo('template_url'); ?>/img/logo-web-hi.png" class="img-responsive center-block">
+		<h1>Administradora de Condomínio</h1>
+	</a>
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav nav-pills nav-stacked">
+			<li><a hreh='#'>Administração</a></li>
+			<li><a href="#">Diferencial</a></li>
+			<li><a href="#">Faça um simulação</a></li>
+			<li class="active"><a href="#">Solicite um proposta</a></li>
+		</ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+</section>
+<section id="meio" class="col-sm-10 col-sm-offset-2 proposta">
+	<h2>Proposta sem compromisso</h2>
+	<!-- conteúdo -->
+	<blockquote>Para ganhar tempo e para que nossa equipe conheça melhor seu condomínio preencha o formulário abaixo com as informações que você achar necessárias e entraremos em contato o mais rápido possível.</blockquote>
+	<div class="well">
+		<h2>OU LIGUE (11)3106-4817 das 9:00 às 17:00</h2>
+	</div>
+
+<? get_footer(); ?>
