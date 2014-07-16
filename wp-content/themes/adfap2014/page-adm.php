24577243a7b2bc9123d3fd0d5a5e09f09c6c1766
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-07-16 14:11:31 -0300

    dinamiza page-adm close #8

--- wp-content/themes/adfap2014/page-adm.php
+++ wp-content/themes/adfap2014/page-adm.php
@@ -6,144 +6,58 @@ get_header('adm');
 ?>
 
 <section id="meio" class="col-sm-7 col-sm-offset-2">
-	<h2>Administradora de Condomínio</h2>
-	<!-- conteúdo -->
-	<p>Ser um real parceiro do síndico é nosso objetivo, muito mais do que administrar a contabilidade, pessoal, pagamentos e recebimentos, criamos uma relação pro-ativa com o síndico, conselheiros e moradores, levando soluções e oportunidades que fazem sentido para as características únicas de seu condomínio.</p>
-	<p>Através da internet, o síndico possui total controle sobre sua gestão,  acompanhando em tempo real cada ação de nossa equipe dedicada. Também pela internet cada morador dispõe de uma série de recursos que facilitam a interação com seus vizinhos, contato com o síndico e conselheiros e também acessam todas as informações contábeis, documentos e atas com total transparência, e principalmente a facilidade de gerar segunda via de boletos e a opção de pagar diretamente pela internet.</p>
-	<blockquote>Entender as características únicas do seu condomínio é o que nos permite oferecer uma administração eficiente e ágil.</blockquote>
-
-	<h2>Serviços especializados</h2>
-	<div class="accordion" id="accordion1">
-		<div class="accordion-group">
-			<div class="accordion-heading">
-				<a class="accordion-toggle" href="#serv1" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>Gestão Administrativa</a>
-			</div>
-			<div class="accordion-body collapse" id="serv1">
-				<div class="accordion-inner">Através da nossa equipe altamente qualificada e nossos recursos tecnológicos a administração do seu condomínio é descomplicada ao máximo, organizando os documentos, o cadastro dos proprietários, controle de prazos e responsabilidades legais, transcrição de atas, coleta de orçamentos, transparência aos condôminos e etc...</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv2" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>Departamento Pessoal</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv2">
-				<div class="accordion-inner">Nada é mais complicado do que controlar toda documentação e burocracia de funcionários, nossa equipe especializada cuida de todo processo necessário para a contratação, gestão e desligamento de seus funcionários, além é lógico de auxiliar na escala, levando em consideração as particularidades de cada Condomínio.</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv3" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>
-			Assessoria Judídica
-			</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv3">
-				<div class="accordion-inner">Entender os meandros das leis trabalhistas e outras relacionadas a condomínios é parte importante para evitar processos. Nossa equipe jurídica está sempre pronta e atualizada para tirar todas as dúvidas e orientar o síndico e condôminos para a melhor solução custo benefício.</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv4" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>
-			Apoio total ao Síndico
-			</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv4">
-			<div class="accordion-inner">Auxiliar o síndico nas novidades referentes ao mercado de condomínios, novidades em segurança, maquinários, leis, manutenção predial e etc... são inúmeras as formas como apoiamos o síndico para que ele possa se dedicar à gestão humana do condomínio criando um ambiente harmonioso de bons vizinhos.</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv5" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>
-			Cobrança
-			</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv5">
-				<div class="accordion-inner">Não basta emitir os boletos, nossa equipe é preparada para acompanhar os pagamentos dos condomínios e apontar os devedores e quais os melhores procedimentos de cobrança.</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv6" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>
-			Contas a Pagar
-			</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv6">
-			<div class="accordion-inner">Lembrar a data correta de pagar os fornecedores toma muito tempo e cuidado, nosso departamento financeiro não só cuida disso como também está treinado para as mais novas soluções em pagamento eletrônico de todos os bancos, mantendo todo o controle e transparência dos pagamentos com o síndico mas com a facilidade de pagamentos pela internet. Nossa equipe está apta a  treinar o sindico no uso destas ferramentas.</div>
-			</div>
-			</div>
-		<div class="accordion-group">
-			<div class="accordion-heading"><a class="accordion-toggle" href="#serv7" data-toggle="collapse" data-parent="#accordion1"><h2><span class='label label-default'>
-			Controle de moradores
-			</span></h2></a></div>
-			<div class="accordion-body collapse" id="serv7">
-			<div class="accordion-inner">Quanto mais o tempo passa, mais os vizinhos mudam. São novos moradores, uns locatários, outros proprietários, outros funcionários de empresas e etc... Mantemos os cadastros completos e atualizados e ainda fornecemos um canal de fácil comunicação com nossa equipe através do nosso site para pedidos de alterações cadastrais.
-			</div>
-			</div>
-		</div>
-	</div>
-	<div class="well">
-		<h3>Solicite uma proposta</h3>
-		<form class="form-horizontal" role='form'>
-			<div class="col-sm-8 col-sm-offset-2">
-				<div class="form-group">
-					<label for="nome" class="col-sm-2 control-label">Nome</label>
-					<div class="col-sm-10">
-						<input type="text" class="form-control" id="inputEmail3">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="endereco" class="col-sm-2 control-label">Endereço</label>
-					<div class="col-sm-10">
-						<input type="text" class="form-control" id="inputEmail3">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="cep" class="col-sm-2 control-label">CEP</label>
-					<div class="col-sm-10">
-						<input type="text" class="form-control" id="inputEmail3" placeholder="99999-999">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="email" class="col-sm-2 control-label">Email</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control" id="inputEmail3">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="comentario" class="col-sm-2 control-label">Comentário</label>
-					<div class="col-sm-10">
-						<textarea class="form-control" rows="3"></textarea>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-12">
-						<button type="submit" class="btn btn-lg btn-success btn-block">ENVIAR</button>
-					</div>
-				</div>
-			</div>
-		</form>
-	</div>
-
-
+<?php
+	if  ( have_posts() ) :
+		while ( have_posts() ) : the_post();
+			the_content();
+		endwhile;
+	else:
+		echo "não achou pagina";
+	endif;
+?>
 
 </section>
 <section id='direita' class='col-sm-3'>
 	<h3 class="sub-titulo">Blogs</h3>
 	<ul class="nav nav-pills nav-stacked">
-		<li>
-			<a href="#">
-				<img src="http://lorempixel.com/40/40/city/9/" class="thumbnail pull-left">
-				<h3>Lorem ipsum dolor sit amet orci aliquam.</h3>
-			</a>
-		</li>
-		<li>
-			<a href="#">
-				<img src="http://lorempixel.com/40/40/city/9/" class="thumbnail pull-left">
-				<h3>Lorem ipsum dolor sit amet orci aliquam.</h3>
-			</a>
-		</li>
-		<li>
-			<a href="#">
-				<img src="http://lorempixel.com/40/40/city/9/" class="thumbnail pull-left">
-				<h3>Lorem ipsum dolor sit amet orci aliquam.</h3>
-			</a>
-		</li>
-		<li>
-			<a href="#">
-				<img src="http://lorempixel.com/40/40/city/9/" class="thumbnail pull-left">
-				<h3>Lorem ipsum dolor sit amet orci aliquam.</h3>
-			</a>
-		</li>
+<?php
+	$args = array(
+	    'numberposts'	=> 4,
+	    'offset'		=> 0,
+	    'category' 		=> 3,
+	    'orderby' 		=> 'post_date',
+	    'order' 		=> 'DESC',
+	    'post_type' => 'post',
+	    'post_status' => 'publish',
+	    'suppress_filters' => true );
+	$recent_posts = wp_get_recent_posts ($args, ARRAY_A);
 
+	$count_img = rand(1,9);
+
+	foreach ($recent_posts as $post) {
+		//verifica sé há thumbnail
+		if ( $count_img >= 9 ) {
+			$count_img = 1;
+		}else{
+			$count_img++;
+		}
+		if ( has_post_thumbnail($post["ID"]) ) {
+			$img_post = get_the_post_thumbnail($post["ID"], 'thumbnail' , array('class' => 'thumbnail pull-left'));
+		}
+		else {
+			$img_post = '<img src="http://lorempixel.com/50/50/city/'.$count_img.'" class="thumbnail pull-left wp-post-image" alt="random image" />';
+		}
+	?>
+		<li>
+			<a href="<?php echo get_permalink($post["ID"]); ?>">
+				<?php echo $img_post; ?>
+				<h3><?php echo esc_attr($post["post_title"]); ?></h3>
+			</a>
+		</li>
+		<?php		
+	}
+
+	?>
 	</ul>
 </section>
 
